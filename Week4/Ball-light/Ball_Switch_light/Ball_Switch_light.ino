ffc7565be17168914529a3f575f8a3ea0a86a976
Serene Xiaopu Deng <xiaopudeng@cca.edu>
2019-02-21 07:29:51 -0800

    Upload

---
+++ Week4/Ball-light/Ball_Switch_light/Ball_Switch_light.ino
@@ -0,0 +1,28 @@
+//www.elegoo.com
+//2016.12.08
+/*****************************************/
+const int ledPin = 10;//the led attach to
+int digitalVal;
+void setup()
+{ 
+  pinMode(ledPin,OUTPUT);//initialize the ledPin as an output
+  pinMode(2,INPUT);
+  digitalWrite(2, HIGH);
+} 
+/******************************************/
+void loop() 
+{  
+
+
+digitalVal = digitalRead(2);
+    Serial.println(digitalVal);
+  if(HIGH == digitalVal)
+  {
+    digitalWrite(ledPin,HIGH);//turn the led off
+  }
+  else
+  {
+    digitalWrite(ledPin,LOW);//turn the led on 
+  }
+}
+/**********************************************/
